05bd5713ef9896968ddb1ddad64358913ee6e930
Thomas Pepio <tpepio@octo.com>
2018-09-30 21:08:30 +0200

    Prepare for the haskell kata

--- src/Endpoints.hs
+++ src/Endpoints.hs
@@ -26,28 +26,10 @@ server :: Server ItemAPI
 server = getItem :<|> postItem
 
 getItem :: Integer -> Handler Item
-getItem id = do
-    item <- liftIO $ findItemById id
-    case item of
-        DoesNotExists -> throwError $ err404 { errBody = "This item does not exists" }
-        NotAvailable  -> throwError $ err404 { errBody = "This item is not in stock" }
-        Available i      -> return i
+getItem = undefined
 
 postItem :: Item -> Handler ItemId
-postItem itemToPost = do
-    newItem <- liftIO $ addNewItem itemToPost
-    case newItem of
-        Right _  -> return 1 -- TODO : return the ID of the new item instead (is ItemAvailability a good design choice ?)
-        Left err -> throwError $ err400 { errBody = "This item already exists" }
+postItem = undefined
 
 application :: Application
 application = serve itemAPI server
-
-instance FromJSON Item where
-    parseJSON = withObject "Item" $ \i -> Item
-        <$> i .: "id"
-        <*> i .: "name"
-        <*> i .: "description"
-
-instance ToJSON Item where
-    toJSON (Item id name desc) = object [ "id" .= id, "name" .= name, "description" .= desc ]
--- src/Persistence.hs
+++ src/Persistence.hs
@@ -22,48 +22,14 @@ import           System.Environment
 import           Environment
 import           Item
 
-instance FromRow Item where
-    fromRow = Item <$> field <*> field <*> field
-
-instance FromRow Stock where
-    fromRow = Stock <$> liftM3 Item field field field <*> field
-
-connection :: IO Connection
-connection = do
-    (Config dbhost dbuser dbpassword dbname) <- readConfiguration
-    connect defaultConnectInfo {
-        connectHost = dbhost,
-        connectPort = 5432,
-        connectUser = dbuser,
-        connectPassword = dbpassword,
-        connectDatabase = dbname
-    }
+connection :: IO Connection -- How to get a connection from postgres-simple ?
+connection = undefined
 
 findItemById :: ItemId -> IO ItemAvailability
-findItemById id = do
-    conn <- liftIO connection
-    stock <- query conn "select id, name, description, quantity from item where id=?" [id]
-    case stock of
-        [Stock item qt] ->
-            if qt > 0
-                then return $ Available item
-                else return NotAvailable
-        [] -> return DoesNotExists
+findItemById = undefined
 
 findItemByNameAndDescription :: Item -> IO ItemAvailability
-findItemByNameAndDescription (Item _ name desc) = do
-    conn <- liftIO connection
-    item <- query conn "select id, name, description from item where name=? and description=?" [name, desc]
-    case item of
-        [i] -> return $ Available i
-        []  -> return $ DoesNotExists
+findItemByNameAndDescription = undefined
 
 addNewItem :: Item -> IO (Either Text Int64)
-addNewItem (Item id name desc) = do
-    conn <- liftIO connection
-    itemExists <- findItemByNameAndDescription (Item id name desc)
-    case itemExists of
-        DoesNotExists -> do
-            nbRows <- execute conn "insert into item(name, description, quantity) values(?, ?, 0)" [name, desc]
-            return $ Right nbRows
-        _ -> return $ Left "Item already exists"
+addNewItem = undefined
